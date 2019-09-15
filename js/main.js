28e0332ec8aaf41d71da06f726ff65b6d9ef3d5c
emmaglorypraise <emmaglorypraise@gmail.com>
2019-09-15 19:55:33 +0100

    files upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+
+	// function initHomeSlider()
+	// {
+	// 	if($('.home_slider').length)
+	// 	{
+	// 		var homeSlider = $('.home_slider');
+	// 		homeSlider.owlCarousel(
+	// 		{
+	// 			items:1,
+	// 			loop:true,
+	// 			autoplay:true,
+	// 			nav:false,
+	// 			dots:false,
+	// 			smartSpeed:1200
+	// 		});
+
+	// 		if($('.home_slider_prev').length)
+	// 		{
+	// 			var prev = $('.home_slider_prev');
+	// 			prev.on('click', function()
+	// 			{
+	// 				homeSlider.trigger('prev.owl.carousel');
+	// 			});
+	// 		}
+
+	// 		if($('.home_slider_next').length)
+	// 		{
+	// 			var next = $('.home_slider_next');
+	// 			next.on('click', function()
+	// 			{
+	// 				homeSlider.trigger('next.owl.carousel');
+	// 			});
+	// 		}
+	// 	}
+	// }
